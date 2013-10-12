88e212267dbc4203973a0a6612ee17fa2cf617d8
Michael Guntsche <mike@it-loops.com>
2013-10-12 13:14:24 +0200

    Disable digest authentication

--- misc/config.php
+++ misc/config.php
@@ -197,7 +197,7 @@
 	// if you experience weird errors and tt-rss failing to start, blank pages
 	// after login, or content encoding errors, disable it.
 
-	define('PLUGINS', 'auth_internal, note, digest');
+	define('PLUGINS', 'auth_internal, note');
 	// Comma-separated list of plugins to load automatically for all users.
 	// System plugins have to be specified here. Please enable at least one
 	// authentication plugin here (auth_*).
