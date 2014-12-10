e7d098a6c8a706b164deb2f0191552697455d545
Michael Guntsche <mike@it-loops.com>
2014-12-10 08:09:34 -0500

    Enable caching of starred Images per defautlt
    This is a default setting for this instance now.

--- misc/config.php
+++ misc/config.php
@@ -203,7 +203,7 @@
 	// if you experience weird errors and tt-rss failing to start, blank pages
 	// after login, or content encoding errors, disable it.
 
-	define('PLUGINS', 'auth_internal, note, videoframes, search_sphinx');
+	define('PLUGINS', 'auth_internal, note, videoframes, search_sphinx, cache_starred_images');
 	// Comma-separated list of plugins to load automatically for all users.
 	// System plugins have to be specified here. Please enable at least one
 	// authentication plugin here (auth_*).
